f01797009dfb5138e71859389f6ab2af021e344d
akshaynirmal19 <110170201+akshaynirmal19@users.noreply.github.com>
2022-07-28 17:13:26 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+
+
+
+function App(){
+
+    return(     
+    <>
+   
+
+    <div className="a">
+    <div className="m_s">
+
+    <img src="R.jpg" alt="myPic"/>
+
+<h3>About Us</h3>
+<h3>Services</h3>
+<h3 e>Contact Us</h3> 
+
+</div><br/><br/>
+
+    
+    <h1>Reasons
+
+I'm excited to<br/> learn React</h1><br/><br/>
+
+<ol>
+
+    <li>React makes it painless to create interactive UIs. Design simple views for each state in your application, and React will efficiently update and render just the right components when your data changes.</li>
+    <li>React can also render on the server using Node and power mobile apps using React Native.</li>
+</ol>
+
+<div className="footer">
+
+<h3 className="h3"> © 2022 nirmal development. All rights reserved.</h3>
+
+</div>
+
+
+    </div>
+
+
+
+    </>
+    )
+}
+export default App;
